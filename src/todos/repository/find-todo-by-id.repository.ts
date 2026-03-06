1d3edca369569fff17dad0038ee7f95e8667842a
ofranath <nathanfraga@yahoo.com>
2026-03-06 11:03:35 -0400

    update

---
+++ src/todos/repository/find-todo-by-id.repository.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/shared/databases/prisma.database";
+@Injectable()
+export class FindTodoRepository {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async execute(id: string) {
+        return await this.prisma.todo.findUnique({ where: { id } });
+    }
+}
